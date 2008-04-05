10154eab2d9affbe97a49d0c1edba847a5ac861d
Tony <tony@neuromancer>
2008-04-05 18:05:12 +0100

    savepoint

--- main/groovy/demo/SimpleBeanExample.groovy
+++ main/groovy/demo/SimpleBeanExample.groovy
@@ -6,9 +6,11 @@ public class SimpleGroovyBean {
 }
 
 // First with the Groovy bean
-def gbean = new SimpleGroovyBean()
-gbean.setPropertyA("A")
-gbean.setPropertyB(1)
+//   def gbean = new SimpleGroovyBean()
+//   gbean.setPropertyA("A")
+//   gbean.setPropertyB(1)
+def gbean = new SimpleGroovyBean(propertyA: "A", propertyB: 1)
+
 
 // Double equals equivalent to .equals()
 assert gbean.propertyA == "A"
@@ -19,16 +21,10 @@ System.out.println("Property A: ${gbean.propertyA}");
 System.out.println("Property B: ${gbean.propertyB}");
 
 // Now with the Java bean
-def jbean = new SimpleJavaBean()
-jbean.setPropertyA("A")
-jbean.setPropertyB(1)
+def jbean = new SimpleJavaBean(propertyA: "A", propertyB: 1)
 
-// Double equals equivalent to .equals()
 assert jbean.propertyA == "A"
 assert jbean.propertyB == 1
 
-// GStrings support expansion
 System.out.println("Property A: ${jbean.propertyA}");
 System.out.println("Property B: ${jbean.propertyB}");
-
-
